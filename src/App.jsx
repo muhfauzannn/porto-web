9d7c3a9d188d409b539549a2b88299c4ad546783
Muhammad Fauzan <muhammadfauzan@Muhammads-MacBook-Air.local>
2024-10-27 19:56:52 +0700

    add some project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
 import Hero from './components/hero'
 import About from './components/about'
 import Project from './components/project'
 import Footer from './components/footer'
 import dataProject from './components/data'
-
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Muhammad Fauzan Portofolio</title>
+      </Helmet>
       <Hero />
       <About />
-      <Project 
-        data = {dataProject}
-      />
+      <Project data={dataProject} />
       <Footer />
     </>
   )
-};
+}
 
 export default App
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -3,7 +3,9 @@ import nodejsLogo from '/nodejs.svg'
 import tailwindLogo from '/tailwind.svg'
 import figmaLogo from '/figma.svg'
 import postgressLogo from '/postgress.svg'
+import nextjsLogo from '/nextjsLogo.png'
 import shurkou from '/shurkou.png'
+import csui24 from '/csui.png'
 
 
 const dataProject = [
@@ -16,11 +18,19 @@ const dataProject = [
       ]
     },
     {
-      title: "Shurkou Store - Website (PROGRESS)",
+      title: "Shurkou Store - Website",
       picture: shurkou,
       desc: "I contributed as a UI/UX designer and full-stack developer. The site includes subscription services, login and signup features, and payment gateway integration (in progress).",
       buildWith : [
         figmaLogo,nodejsLogo,tailwindLogo,postgressLogo
+      ]
+    },
+    {
+      title: "CSUI 2024 - Website",
+      picture: csui24,
+      desc: "CSUI24 is a company profile website for class of 2024 majoring in Computer Science at University of Indonesia. I contributed as a Front End Developers during development of this site.",
+      buildWith : [
+        tailwindLogo,nextjsLogo
       ]
     }
   ];
